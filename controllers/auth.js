f2724f3bd0eec5ab3c1966ceb12197e6601fcbb5
Dariusz Okoński <darek200180@gmail.com>
2024-06-06 19:39:28 +0200

    mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,20 @@ const { StatusCodes } = require('http-status-codes');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  // TODO: this hashing functionality is moved to User model
+  // const { name, email, password } = req.body;
   // if (!name || !email || !password) {
   //   throw new BadRequestError('Please provide name, email and password');
   // }
 
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  // const salt = await bcrypt.genSalt(10);
+  // const hashedPassword = await bcrypt.hash(password, salt);
 
-  const tempUser = { name, email, password: hashedPassword };
+  // const tempUser = { name, email, password: hashedPassword };
 
-  const user = await User.create({ ...tempUser });
+  // const user = await User.create({ ...tempUser });
+
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
