8d3ab047519a5239d9a810e60185609945da3370
Dariusz Okoński <darek200180@gmail.com>
2024-06-06 19:49:27 +0200

    add token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,17 @@
 const { BadRequestError } = require('../errors');
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   // TODO: this hashing functionality is moved to User model
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   throw new BadRequestError('Please provide name, email and password');
-  // }
-
-  // const salt = await bcrypt.genSalt(10);
-  // const hashedPassword = await bcrypt.hash(password, salt);
-
-  // const tempUser = { name, email, password: hashedPassword };
-
-  // const user = await User.create({ ...tempUser });
-
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
